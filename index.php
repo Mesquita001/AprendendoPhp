70e55dc1b42539f60bc06f1eb72607d5a15d80ae
Andre Mesquita <mesquitas@hotmail.com>
2021-09-29 23:07:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
 echo 'Hello World';
+
+
+class Main
+{
+    public function __construct()
+    {
+        echo "Hello\n";
+
+    }
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     * @return void
+     */
+    public function teste(string $name): void
+    {}
+
+
+}
+(new Main ())->teste('Andre');
